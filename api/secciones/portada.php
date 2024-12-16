68a7e41978440bd24c64aa90a8ee3c9cf7be53eb
ab005 <90392047+ab005@users.noreply.github.com>
2024-12-16 04:06:58 +0100

    Cambio final del título de la portada

--- api/secciones/portada.php
+++ api/secciones/portada.php
@@ -4,7 +4,7 @@ SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
 
 $portada = [
-"titulo" => "Portada actualizada", (1)
+"titulo" => "Portada actualizada",
 "autor" => "ab005",
 "resumen" => "Resumen de la portada",
 ];
